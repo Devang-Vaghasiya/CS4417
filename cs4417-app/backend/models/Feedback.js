4532df0ac434a6708b32cf46620d3e4f8d5e78ff
Choeings <schoeingseldon2003@gmail.com>
2026-03-18 18:19:03 -0300

    added script for models

---
+++ cs4417-app/backend/models/Feedback.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose');  const feedbackSchema = new mongoose.Schema({   userId:  { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },   message: { type: String, required: true, maxlength: 1000 }, }, { timestamps: true });  module.exports = mongoose.model('Feedback', feedbackSchema);
